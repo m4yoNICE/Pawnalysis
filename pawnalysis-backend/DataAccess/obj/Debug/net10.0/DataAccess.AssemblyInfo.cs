f04ad38010ce8ca8332e38528925674f802d1762
m4yoNICE <cenizaroswellrey@gmail.com>
2026-07-21 15:20:01 +0800

    [fix][Roswell] remove incorrect eval sign-flip, consolidate LLM to whole-game summary, async Groq client

--- pawnalysis-backend/DataAccess/obj/Debug/net10.0/DataAccess.AssemblyInfo.cs
+++ pawnalysis-backend/DataAccess/obj/Debug/net10.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b513dcfb1ed2eb21aa3b130fa22ff68bc55d8c1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a2d1967dd74ac050352cfac8a9d5d84e0111653")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
